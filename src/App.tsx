ab2c72a87695b6ab27d30fb24a11af0aa16d251e
Bruno9t <bru_acad@outlook.com>
2020-07-21 19:09:34 -0300

    troca de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {ThemeProvider,DefaultTheme} from 'styled-components'
+import light from './styles/themes/light'
+import dark from './styles/themes/dark'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import usePersitedState from './utils/usePersitedState'
+
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+
+
+const App = ()=>{
+    const [theme,setTheme] = usePersitedState<DefaultTheme>('theme',light)
+
+    const toggleTheme = ()=>{
+        setTheme(theme.title === 'light' ? dark:light)
+    }
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <GlobalStyle />
+                <Header toggleTheme={toggleTheme} />
+            </div>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
